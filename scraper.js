8c7d1e306f5d964767a8cb9f84bd903fd8dfe341
zippiertomcat <christianhuerta309@gmail.com>
2023-06-29 18:38:38 -0700

    added photos, business name, and total review

--- scraper.js
+++ scraper.js
@@ -23,6 +23,8 @@ async function scrapeYelpReviews(link, start = 0, end = null) {
   const startReview = start;
   let endReview = end;
 
+  let businessName;
+  let totalRating;
   // Create Google Cloud Storage object
   const googleCloud = new Storage({
     keyFilename: 'path/to/keyfile.json',
@@ -43,15 +45,22 @@ async function scrapeYelpReviews(link, start = 0, end = null) {
   await getHTML(url).then((res) => {
     const $ = cheerio.load(res);
     reviewAmountString = $(`body > yelp-react-root > div:nth-child(1) > div.photoHeader__09f24__nPvHp.border-color--default__09f24__NPAKY > div.photo-header-content-container__09f24__jDLBB.border-color--default__09f24__NPAKY > div.photo-header-content__09f24__q7rNO.padding-r2__09f24__ByXi4.border-color--default__09f24__NPAKY > div > div > div.arrange__09f24__LDfbs.gutter-1-5__09f24__vMtpw.vertical-align-middle__09f24__zU9sE.margin-b2__09f24__CEMjT.border-color--default__09f24__NPAKY > div.arrange-unit__09f24__rqHTg.arrange-unit-fill__09f24__CUubG.border-color--default__09f24__NPAKY.nowrap__09f24__lBkC2 > span > a`).text();
+    //gets business name
+     businessName = $('body > yelp-react-root > div:nth-child(1) > div.photoHeader__09f24__nPvHp.border-color--default__09f24__NPAKY > div.photo-header-content-container__09f24__jDLBB.border-color--default__09f24__NPAKY > div.photo-header-content__09f24__q7rNO.padding-r2__09f24__ByXi4.border-color--default__09f24__NPAKY > div > div.arrange-unit__09f24__rqHTg.arrange-unit-fill__09f24__CUubG.border-color--default__09f24__NPAKY > div.headingLight__09f24__N86u1.margin-b1__09f24__vaLrm.border-color--default__09f24__NPAKY > h1').text();
+    //gets total rating
+    totalRating = $('body > yelp-react-root > div:nth-child(1) > div.photoHeader__09f24__nPvHp.border-color--default__09f24__NPAKY > div.photo-header-content-container__09f24__jDLBB.border-color--default__09f24__NPAKY > div.photo-header-content__09f24__q7rNO.padding-r2__09f24__ByXi4.border-color--default__09f24__NPAKY > div > div.arrange-unit__09f24__rqHTg.arrange-unit-fill__09f24__CUubG.border-color--default__09f24__NPAKY > div.arrange__09f24__LDfbs.gutter-1-5__09f24__vMtpw.vertical-align-middle__09f24__zU9sE.margin-b2__09f24__CEMjT.border-color--default__09f24__NPAKY > div:nth-child(1) > span > div').attr('aria-label');
   });
   reviewAmount = parseInt(reviewAmountString.replace(/[^0-9]/g, ''), 10);
 
   if (endReview == null) endReview = reviewAmount+9;
-  
+
+
+ 
+
 
   // Scrapes data from each page and adds it to the reviews array
   for (let x = startReview; x < endReview + 1 - 10; x += 10) {
-    await promiseArray.push(scrape(x));
+    await promiseArray.push(await scrape(x));
   }
 
   async function scrape(pageNum) {
@@ -61,7 +70,7 @@ async function scrapeYelpReviews(link, start = 0, end = null) {
 
     let htmlContent = await getHTML(pageURL);
 
-    scrapePage(htmlContent);
+    await scrapePage(htmlContent);
   }
 
   async function scrapePage(res) {
@@ -74,10 +83,45 @@ async function scrapeYelpReviews(link, start = 0, end = null) {
       let date = review.find(`> div > div.margin-t1__09f24__w96jn.margin-b1-5__09f24__NHcQi.border-color--default__09f24__NPAKY > div > div.arrange-unit__09f24__rqHTg.arrange-unit-fill__09f24__CUubG.border-color--default__09f24__NPAKY > span`).text();
       let text = review.find(`> div > div:nth-child(4) > p > span`).text();
       let rating = review.find(`> div > div.margin-t1__09f24__w96jn.margin-b1-5__09f24__NHcQi.border-color--default__09f24__NPAKY > div > div:nth-child(1) > span > div`).attr('aria-label');
+      let imageNum = $(`#reviews > section > div:nth-child(2) > ul > li:nth-child(${reviewNum}) > div > div:nth-child(3) > div > span > a`).text();
+      let images = [];
+      //if there are images in review, grabs url for each image and adds to array
+      if(imageNum.length > 0){
+        imageNum = parseInt(imageNum.match(/\d+/)[0]);
+        for(let i = 1; i <= imageNum; i++){
+        let imageUrl = $(`#reviews > section > div:nth-child(2) > ul > li:nth-child(${reviewNum}) > div > div.margin-t3__09f24__riq4X.margin-b2__09f24__CEMjT.border-color--default__09f24__NPAKY > div > div:nth-child(${i}) > div > div > div > a > img`).attr('src')
+         
+        //if too many images in review, go to reviewer page instead and get all images from there
+        if(imageUrl==undefined){
+          images = [];
+           let reviewerPageURL =  await $(`#reviews > section > div:nth-child(2) > ul > li:nth-child(${reviewNum}) > div > div.margin-t3__09f24__riq4X.margin-b2__09f24__CEMjT.border-color--default__09f24__NPAKY > p > a`).attr('href');
+          images = await getReviewerPageImages(reviewerPageURL,imageNum);
+           
+          break;
+        } 
+         images.push(imageUrl);
+        }
+       
+      }         
+
+     
 
       if (name == '' || name == undefined) continue;
-      reviews.push({ name, location, rating, text, date });
+      reviews.push({ name, location, rating, text, date,images });
     }
+  }
+
+  async function getReviewerPageImages(reviewerPageLink, amountOfImages){
+    const userPage = await axios.get('https://www.yelp.com/'+reviewerPageLink);
+           const html2 = userPage.data;
+           const $2 = cheerio.load(html2);
+           let newImages = [];
+           for(let z = 1; z <= amountOfImages; z++){
+            let imageProfileUrl = $2(`#super-container > div.container > div > div.media-landing.js-media-landing > div.media-landing_gallery.photos > ul > li:nth-child(${z}) > div > img`).attr('src');
+             newImages.push(imageProfileUrl);
+          }
+
+return newImages;
   }
 
   // Run the scraper for all pages
@@ -96,9 +140,9 @@ async function scrapeYelpReviews(link, start = 0, end = null) {
   }
 
   // Runs the upload function
-  uploadFile(JSON.stringify({ endReview, reviews })).catch(console.error);
+  uploadFile(JSON.stringify({ endReview,businessName,totalRating, reviews })).catch(console.error);
 
-  return JSON.stringify({endReview,reviews});
+  return JSON.stringify({ endReview,businessName,totalRating, reviews });
 }
 
 module.exports = scrapeYelpReviews;
